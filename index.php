658db2a75dd0f155a37042985d051a5688ab8ae6
guilherme barbosa <guilhermebali@gmail.com>
2014-11-10 19:04:10 -0200

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<form action="upload_file.php" method="post" enctype="multipart/form-data">
+Enviar Arquivo:<input type="file" name="arquivo" size="20"><br>
+<input type="submit"  value="Submit">
+</form>
+
+</body>
+</html>
---
+++ upload_file.php
@@ -0,0 +1,20 @@
+<?php
+
+
+   if(isset($_FILES['arquivo']))
+   {
+      date_default_timezone_set("Brazil/East"); //Definindo timezone padrão
+ 
+      $ext = strtolower(substr($_FILES['arquivo']['name'],-4)); //Pegando extensão do arquivo
+      $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+      $dir = 'x/'; //Diretório para uploads
+ 
+      if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $dir.$new_name)){
+		print "Arquivo enviado";
+		}else{
+		print "Arquivo não enviado";
+		}
+   }
+
+
+?>
