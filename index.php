bafc941e766291342facd63fb981eceb4b3a5b10
Guilherme Barbosa <guilhermebali@gmail.com>
2014-11-11 20:17:05 -0200

    pequena alteração

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
 
 <form action="upload_file.php" method="post" enctype="multipart/form-data">
-Enviar Arquivo:<input type="file" name="arquivo" size="20"><br>
+Enviar Arquivo.:<input type="file" name="arquivo" size="20"><br>
 <input type="submit"  value="Submit">
 </form>
 
